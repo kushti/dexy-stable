5764c607f93797773f3b69e677d7c093e60c9a84
kushti <alex.chepurnoy@iohk.io>
2022-05-17 18:40:17 +0300

    initial paper commit

---
+++ spec/paper/compile.sh
@@ -0,0 +1,14 @@
+rm dexy.aux
+rm dexy.out
+rm dexy.log
+rm dexy.bbl
+rm dexy.blg
+pdflatex dexy
+bibtex dexy
+pdflatex dexy
+pdflatex dexy
+rm dexy.aux
+rm dexy.out
+rm dexy.log
+rm dexy.bbl
+rm dexy.blg
