1e97cbe371f95ab458e3f2dae305fc59ef2cafdc
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2023-03-15 23:35:14 +0300

    oracle refresh test

--- src/test/scala/dexy/BuybackSpec.scala
+++ src/test/scala/dexy/BuybackSpec.scala
@@ -1,21 +1,57 @@
 package dexy
 
-import kiosk.ergo.{DhtData, KioskBox}
+import kiosk.ergo.{DhtData, KioskBox, KioskInt, KioskLong}
 import kiosk.tx.TxUtil
-import org.ergoplatform.appkit.{BlockchainContext, HttpClientTesting, InputBox}
+import oracles.OracleHelpers
+import org.ergoplatform.appkit.{BlockchainContext, ContextVar, HttpClientTesting, InputBox}
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-class BuybackSpec extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks with HttpClientTesting with Common {
+class BuybackSpec extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks
+  with HttpClientTesting with Common with OracleHelpers {
+
+  import oracles.OracleContracts._
 
   val ergoClient = createMockedErgoClient(MockData(Nil, Nil))
 
   property("giveback scenario") {
     ergoClient.execute { implicit ctx: BlockchainContext =>
 
-      val inputs = Array[InputBox]()
+      val refreshBox = bootstrapRefreshBox()
+      val poolBox = bootstrapPoolBox(ctx.getHeight - config.epochLength - 1, 1)
+
+      val oracleBox1 = bootstrapOracleBox(pubKey1, 10)
+      val oracleBox2 = bootstrapOracleBox(pubKey2, 20)
+      val oracleBox3 = bootstrapOracleBox(pubKey3, 30)
+      val oracleBox4 = bootstrapOracleBox(pubKey4, 40)
+      val oracleBox5 = bootstrapOracleBox(pubKey5, 50)
+
+      val dataPoint1 = createDataPoint(1000, 0, oracleAddress, minStorageRent, oracleBox1, privKey1, 0, 10)
+      val dataPoint2 = createDataPoint(1001, 0, oracleAddress, minStorageRent, oracleBox2, privKey2, 0, 20)
+      val dataPoint3 = createDataPoint(1002, 0, oracleAddress, minStorageRent, oracleBox3, privKey3, 0, 30)
+      val dataPoint4 = createDataPoint(1003, 0, oracleAddress, minStorageRent, oracleBox4, privKey4, 0, 40)
+      val dataPoint5 = createDataPoint(1004, 0, oracleAddress, minStorageRent, oracleBox5, privKey5, 0, 50)
+
+      val inputs = Array[InputBox](
+        poolBox,
+        refreshBox.withContextVars(new ContextVar(0, KioskInt(0).getErgoValue)), // 1st dataPoint box (dataPoint1) is spender
+        dataPoint1.withContextVars(new ContextVar(0, KioskInt(2).getErgoValue)), // output index 2 corresponds to dataPoint1
+        dataPoint2.withContextVars(new ContextVar(0, KioskInt(3).getErgoValue)), // output index 3 corresponds to dataPoint2
+        dataPoint3.withContextVars(new ContextVar(0, KioskInt(4).getErgoValue)), // output index 4 corresponds to dataPoint3
+        dataPoint4.withContextVars(new ContextVar(0, KioskInt(5).getErgoValue)), // output index 5 corresponds to dataPoint4
+        dataPoint5.withContextVars(new ContextVar(0, KioskInt(6).getErgoValue)), // output index 6 corresponds to dataPoint5
+        dummyFundingBox
+      )
       val dataInputs = Array[InputBox]()
-      val outputs = Array[KioskBox]()
+      val outputs = Array[KioskBox](
+        KioskBox(poolAddress, minStorageRent, Array(KioskLong(1002), KioskInt(1)), Array((config.poolNFT, 1), (rewardTokenId, 1000000L - 10))),
+        KioskBox(refreshAddress, minStorageRent, Array.empty, Array((config.refreshNFT, 1))),
+        KioskBox(oracleAddress, minStorageRent, Array(pubKey1), Array((DexySpec.gort, 1), (rewardTokenId, 16))),
+        KioskBox(oracleAddress, minStorageRent, Array(pubKey2), Array((DexySpec.gort, 1), (rewardTokenId, 21))),
+        KioskBox(oracleAddress, minStorageRent, Array(pubKey3), Array((DexySpec.gort, 1), (rewardTokenId, 31))),
+        KioskBox(oracleAddress, minStorageRent, Array(pubKey4), Array((DexySpec.gort, 1), (rewardTokenId, 41))),
+        KioskBox(oracleAddress, minStorageRent, Array(pubKey5), Array((DexySpec.gort, 1), (rewardTokenId, 51)))
+      )
 
       noException shouldBe thrownBy {
         TxUtil.createTx(
---
+++ src/test/scala/oracles/OracleHelpers.scala
@@ -0,0 +1,202 @@
+package oracles
+
+import dexy.{Common, DexySpec}
+import dexy.DexySpec.gort
+import kiosk.ergo.{DhtData, KioskBox, KioskGroupElement, KioskInt, KioskLong}
+import kiosk.tx.TxUtil
+import oracles.OracleContracts.config
+import org.ergoplatform.appkit.{BlockchainContext, ConstantsBuilder, ContextVar, ErgoToken, ErgoValue, InputBox}
+import kiosk.ErgoUtil.{addressToGroupElement => addr2Grp}
+import kiosk.encoding.ScalaErgoConverters.{stringToGroupElement => str2Grp}
+import special.sigma.GroupElement
+
+trait OracleHelpers extends Common {
+
+  private lazy val keyPairs: Array[(KioskGroupElement, BigInt)] = Array(
+    "9eiuh5bJtw9oWDVcfJnwTm1EHfK5949MEm5DStc2sD1TLwDSrpx" -> "37cc5cb5b54f98f92faef749a53b5ce4e9921890d9fb902b4456957d50791bd0",
+    "9f9q6Hs7vXZSQwhbrptQZLkTx15ApjbEkQwWXJqD2NpaouiigJQ" -> "5878ae48fe2d26aa999ed44437cffd2d4ba1543788cff48d490419aef7fc149d",
+    "9fGp73EsRQMpFC7xaYD5JFy2abZeKCUffhDBNbQVtBtQyw61Vym" -> "3ffaffa96b2fd6542914d3953d05256cd505d4beb6174a2601a4e014c3b5a78e",
+    "9fSqnSHKLzRz7sRkfwNW4Rqtmig2bHNaaspsQms1gY2sU6LA2Ng" -> "148bb91ada6ad5e6b1bba02fe70ecd96095e00cbaf0f1f9294f02fedf9855ea0",
+    "9g3izpikC6xuvhnXxNHT1y5nwJNofMsoPiCgr4JXcZV6GUgWPqh" -> "148bb91ada6ad5e6b1bba02fe70ecd96095e00cbaf0f1f9294f02fedf9855ea1",
+    "9g1RasRmLpijKSD1TWuGnCEmBfacRzGANqwHPKXNPcSQypsHCT5" -> "009b74e570880f0a558ed1231c280aff9d8afb2ba238e4ac2ea2d4f5507f01c6ae"
+  ).map { case (address, secret) => KioskGroupElement(str2Grp(addr2Grp(address))) -> BigInt(secret, 16) }
+
+  val pubKey1: KioskGroupElement = keyPairs(0)._1
+  val pubKey2: KioskGroupElement = keyPairs(1)._1
+  val pubKey3: KioskGroupElement = keyPairs(2)._1
+  val pubKey4: KioskGroupElement = keyPairs(3)._1
+  val pubKey5: KioskGroupElement = keyPairs(4)._1
+  val pubKey6: KioskGroupElement = keyPairs(5)._1
+
+  val privKey1: BigInt = keyPairs(0)._2
+  val privKey2: BigInt = keyPairs(1)._2
+  val privKey3: BigInt = keyPairs(2)._2
+  val privKey4: BigInt = keyPairs(3)._2
+  val privKey5: BigInt = keyPairs(4)._2
+  val privKey6: BigInt = keyPairs(5)._2
+
+  val dummyTxId = "f9e5ce5aa0d95f5d54a7bc89c46730d9662397067250aa18a0039631c0f5b809"
+  val dummyBoxId = "5267556B58703273357638792F413F4428472B4B6250655368566D5971337436"
+  val dummyIndex = 1.toShort
+
+  val dummyEpochCounter = 1
+
+  val rewardTokenId = DexySpec.gort
+
+  val dummyScript = "sigmaProp(true)"
+  val junkAddress = "4MQyML64GnzMxZgm" // sigmaProp(true)
+  val dummyNanoErgs = 10000000000000L
+
+  def dummyFundingBox(implicit ctx: BlockchainContext) =
+    ctx // for funding transactions
+      .newTxBuilder()
+      .outBoxBuilder
+      .value(dummyNanoErgs)
+      .contract(ctx.compileContract(ConstantsBuilder.empty(), dummyScript))
+      .build()
+      .convertToInputWith(dummyTxId, dummyIndex)
+
+  def bootstrapOracleBox(pubKey: KioskGroupElement, rewardTokenQty: Long, optRewardTokenId: Option[String] = None, optOracleAddress: Option[String] = None, optOracleTokenId: Option[String] = None)(
+    implicit ctx: BlockchainContext) =
+    TxUtil
+      .createTx(
+        Array(
+          ctx // for funding transactions
+            .newTxBuilder()
+            .outBoxBuilder
+            .value(dummyNanoErgs)
+            .tokens(new ErgoToken(optOracleTokenId.getOrElse(config.oracleTokenId), 1), new ErgoToken(optRewardTokenId.getOrElse(rewardTokenId), rewardTokenQty))
+            .contract(ctx.compileContract(ConstantsBuilder.empty(), dummyScript))
+            .build()
+            .convertToInputWith(dummyTxId, dummyIndex)),
+        Array[InputBox](),
+        Array(
+          KioskBox(
+            optOracleAddress.getOrElse(OracleContracts.oracleAddress),
+            value = minStorageRent,
+            registers = Array(pubKey),
+            tokens = Array((optOracleTokenId.getOrElse(config.oracleTokenId), 1), (optRewardTokenId.getOrElse(rewardTokenId), rewardTokenQty))
+          )),
+        fee = 1000000L,
+        changeAddress,
+        Array[String](),
+        Array[DhtData](),
+        false
+      )
+      .getOutputsToSpend
+      .get(0)
+
+  def bootstrapPoolBox(customCreationHeight: Int,
+                       rate: Long,
+                       counter: Int = 0,
+                       newPoolNFT: Option[String] = None,
+                       optRewardTokenQty: Option[Long] = None,
+                       optRewardTokenId: Option[String] = None)
+                      (implicit ctx: BlockchainContext) =
+    TxUtil
+      .createTx(
+        Array(
+          ctx // for funding transactions
+            .newTxBuilder()
+            .outBoxBuilder
+            .value(dummyNanoErgs)
+            .tokens(
+              new ErgoToken(newPoolNFT.getOrElse(config.poolNFT), 1),
+              new ErgoToken(optRewardTokenId.getOrElse(rewardTokenId), optRewardTokenQty.getOrElse(1000000L))
+            )
+            .contract(ctx.compileContract(ConstantsBuilder.empty(), dummyScript))
+            .build()
+            .convertToInputWith(dummyTxId, dummyIndex)),
+        Array[InputBox](),
+        Array(
+          KioskBox(
+            OracleContracts.poolAddress,
+            value = minStorageRent,
+            registers = Array(KioskLong(rate), KioskInt(counter)),
+            tokens = Array(
+              (newPoolNFT.getOrElse(config.poolNFT), 1),
+              (optRewardTokenId.getOrElse(gort), optRewardTokenQty.getOrElse(1000000L))
+            ),
+            creationHeight = Some(customCreationHeight)
+          )),
+        fee = 1000000L,
+        changeAddress,
+        Array[String](),
+        Array[DhtData](),
+        false
+      )
+      .getOutputsToSpend
+      .get(0)
+
+  def bootstrapRefreshBox(optRefreshAddress: Option[String] = None, optRefreshNFT: Option[String] = None)(implicit ctx: BlockchainContext) =
+    TxUtil
+      .createTx(
+        Array(
+          ctx // for funding transactions
+            .newTxBuilder()
+            .outBoxBuilder
+            .value(dummyNanoErgs)
+            .tokens(new ErgoToken(optRefreshNFT.getOrElse(config.refreshNFT), 1))
+            .contract(ctx.compileContract(ConstantsBuilder.empty(), dummyScript))
+            .build()
+            .convertToInputWith(dummyTxId, dummyIndex)),
+        Array[InputBox](),
+        Array(
+          KioskBox(
+            optRefreshAddress.getOrElse(OracleContracts.refreshAddress),
+            value = minStorageRent,
+            registers = Array.empty,
+            tokens = Array((optRefreshNFT.getOrElse(config.refreshNFT), 1))
+          )),
+        fee = 1000000L,
+        changeAddress,
+        Array[String](),
+        Array[DhtData](),
+        false
+      )
+      .getOutputsToSpend
+      .get(0)
+
+  private def getOracleTokensToAdd(rewardTokenQty: Long, customRewardTokenId: Option[String], optOracleTokenId: Option[String] = None): Array[(String, Long)] = {
+    val oracleTokenIdToUse = optOracleTokenId.getOrElse(config.oracleTokenId)
+    if (rewardTokenQty > 0) Array((oracleTokenIdToUse, 1L), (customRewardTokenId.getOrElse(rewardTokenId), rewardTokenQty)) else Array((oracleTokenIdToUse, 1L))
+  }
+
+  def createDataPoint(dataPointValue: Long,
+                      epochCounter: Int,
+                      outAddress: String,
+                      outValue: Long,
+                      inputOracleBox: InputBox,
+                      privKey: BigInt,
+                      contextVarOutIndex: Int,
+                      rewardTokenQty: Long,
+                      newPubKey: Option[KioskGroupElement] = None,
+                      customCreationHeight: Option[Int] = None,
+                      customRewardTokenId: Option[String] = None,
+                      customOracleTokenId: Option[String] = None)(implicit ctx: BlockchainContext) = {
+    TxUtil
+      .createTx(
+        Array(inputOracleBox.withContextVars(new ContextVar(0, KioskInt(contextVarOutIndex).getErgoValue)), dummyFundingBox),
+        Array[InputBox](),
+        Array(
+          KioskBox(
+            outAddress,
+            value = outValue,
+            registers = Array(
+              newPubKey.getOrElse(KioskGroupElement(inputOracleBox.getRegisters.get(0).asInstanceOf[ErgoValue[GroupElement]].getValue)),
+              KioskInt(epochCounter),
+              KioskLong(dataPointValue)
+            ),
+            tokens = getOracleTokensToAdd(rewardTokenQty, customRewardTokenId, customOracleTokenId),
+            creationHeight = customCreationHeight
+          )),
+        1500000,
+        changeAddress,
+        Array[String](privKey.toString),
+        Array[DhtData](),
+        false
+      )
+      .getOutputsToSpend
+      .get(0)
+  }
+}
